bbf9e7980f60fe6a8b32c9d6e6f3dd3c9686132a
camillab09 <camillab09.kodehode@gmail.com>
2023-10-30 11:27:09 +0100

    Javascript test

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// console.log("Hello World!")
+// Dette er en kommentar
+/*hei
+bla
+bla
+bla
+*/
+let myText = "Dette er tekst."
+console.log(myText)
+let iNeedADrink = "Jeg trenger noe å drikke"
+console.log(iNeedADrink)
+//Dette er en kommentar
+let HelloWorld = "Hello world!"
+console.log(HelloWorld)
+document.getElementById("demo").innerHTML = "This is now in English";
+let howIsThis = "How is this text looking?"
+console.log(howIsThis)
